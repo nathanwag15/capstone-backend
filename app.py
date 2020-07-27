7c357658fe6210f2e2b586338df59ba6677cce21
nathanwagstaff <natewagstaff21@gmail.com>
2020-07-27 10:30:49 -0600

    meh

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
 from flask_heroku import Heroku
+from flask_bcrypt import Bcrypt
 import io
-import os
+
 
 
 
@@ -19,6 +20,7 @@ ma = Marshmallow(app)
 
 heroku = Heroku(app)
 CORS(app)
+bcrypt = Bcrypt(app)
 
 
 class Location(db.Model):
